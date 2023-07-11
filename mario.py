7bdaa5e231e8debbc0705bf05d31f3f628b69977
Aidan Bongiorno <aidanbongiorno@Alessandros-MacBook-Air.local>
2023-07-11 15:44:33 -0400

    mario problem

---
+++ mario.py
@@ -0,0 +1,18 @@
+'''
+This is a python project where your focus is building
+a mario tower.
+
+What you'll learn: Loops, User input, Syntax, and some
+computaation
+
+
+what should it look like: 
+
+    #
+   ##
+  ###
+ ####
+#####
+
+'''
+
