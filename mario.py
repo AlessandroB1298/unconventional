35943068c4055e5634b22a4b7510f8880408b0c3
Aidan Bongiorno <aidanbongiorno@Alessandros-Air.attlocal.net>
2023-07-14 20:51:50 -0400

    solution

--- mario.py
+++ mario.py
@@ -1,5 +1,5 @@
 '''
-This is a python project where your focus is building
+This is a c project where your focus is building
 a mario tower.
 
 What you'll learn: Loops, User input, Syntax, and some
@@ -14,5 +14,19 @@ what should it look like:
  ####
 #####
 
+
 '''
 
+row = 0 
+while True:
+    height = int(input("Height: "))
+    if height >= 1 and height <= 8:
+        break
+space = 1
+    
+for row in range(height):
+    for space in range (height - row -1):
+        print(" ",end='')
+    for hash in range(row):
+        print("#", end ='')
+    print()
